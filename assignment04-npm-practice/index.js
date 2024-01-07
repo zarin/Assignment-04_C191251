6b6e31e9c68cb79a35301b1c731549de4ac31702
Zarin Tasnim Hossain <zaariintaasniim@gmail.com>
2024-01-07 12:38:51 +0600

    done

---
+++ assignment04-npm-practice/index.js
@@ -0,0 +1,21 @@
+// Import NPM Package
+
+// Call appropriate function from the NPM package to solve the assignment
+import { last } from 'underscore';
+/**
+ * Print out the result if ["Java", "Javascript", "Typescript"] this array 
+ * contains "Java"
+ */
+// First Answer 
+const name1 = _.last(["Java", "Javascript", "Typescript"]);
+console.log(name1);
+
+
+
+/**
+ * Print out the result if ["Java", "Javascript", "Typescript"] this array 
+ * contains "C++"
+ */
+// Second Answer
+const name2 = _.last(["C++", "Javascript", "Typescript"]);
+console.log(name2);
